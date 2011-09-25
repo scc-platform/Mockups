58c3606a8e30a483ab1b11d071799de7aa12714b
James <james@jarofgreen.co.uk>
2011-09-25 19:47:43 +0100

    some spacing in links

--- commsPrefs.js
+++ commsPrefs.js
@@ -9,11 +9,11 @@ function editDetails(id) {
 	$('#CommunicateDetails'+id).html('<input type="text" name="details">');
 	$('#CommunicateDetails'+id+' input').val(details[id].detail);
 	
-	$('#CommunicationMethod'+id+' td.actions').html('<input type=submit value="save"><a href="" onclick="cancelEdit('+id+'); return false;">cancel</a>');
+	$('#CommunicationMethod'+id+' td.actions').html('<input type=submit value="save"> <a href="" onclick="cancelEdit('+id+'); return false;">cancel</a>');
 }
 function cancelEdit(id) {
 	/** TODO escapeing **/
 	$('#CommunicateTitle'+id).html(details[id].title);
 	$('#CommunicateDetails'+id).html(details[id].detail);
-	$('#CommunicationMethod'+id+' td.actions').html('<a href="" onclick="editDetails('+id+'); return false;">Edit</a><a href="">Delete</a>');
+	$('#CommunicationMethod'+id+' td.actions').html('<a href="" onclick="editDetails('+id+'); return false;">Edit</a> <a href="">Delete</a>');
 }
