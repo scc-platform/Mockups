097b0180afd6671d3cb3b8e186473c718b133db5
James <james@jarofgreen.co.uk>
2011-09-25 19:32:18 +0100

    when click edit description box changes

--- commsPrefs.js
+++ commsPrefs.js
@@ -2,4 +2,8 @@ function editDetails(id) {
 	var title = $('#CommunicateTitle'+id).text();
 	$('#CommunicateTitle'+id).html('<input type="text" name="title">');
 	$('#CommunicateTitle'+id+' input').val(title);
+	var details = $('#CommunicateDetails'+id).text();
+	$('#CommunicateDetails'+id).html('<input type="text" name="details">');
+	$('#CommunicateDetails'+id+' input').val(details);
+
 }
